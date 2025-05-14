4e7e551b3084c1a3f3b6ec585e329179204362d5
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-14 17:13:07 +0300

    updated custom select

--- src/shared/components/CustomSelect/CustomSelect.tsx
+++ src/shared/components/CustomSelect/CustomSelect.tsx
@@ -1,4 +1,4 @@
-"useclient";
+"use client";
 
 import { ChangeEvent, useState } from "react";
 import Image from "next/image";
@@ -38,22 +38,27 @@ export default function CustomSelect({
           </span>
         )}
       </p>
+
+      {/* Стилізована обгортка */}
       <div
         onClick={() => setIsOpen(!isOpen)}
-        className={`${cls.select} ${bordered && cls.borderedSelect}`}
+        className={`${cls.select} ${bordered ? cls.borderedSelect : ""}`}
       >
         <div className={cls.selected}>
           {value || (
             <p style={{ color: "rgba(17, 24, 28, 0.5)" }}>{placeholder}</p>
           )}
         </div>
+
         <Image
           src="/icons/arrow.svg"
           alt="arrow icon"
           width={16}
           height={16}
-          className={`${cls.svg} ${isOpen && cls.openSvg}`}
+          className={`${cls.svg} ${isOpen ? cls.openSvg : ""}`}
         />
+
+        {/* Якщо відкритий, показуємо випадаючий список */}
         {isOpen && (
           <ul className={cls.dropdown}>
             {options?.map((option) => (
@@ -68,6 +73,25 @@ export default function CustomSelect({
           </ul>
         )}
       </div>
+
+      {/* Нативний селект для валідації */}
+      <select
+        name={id}
+        id={id}
+        required={required}
+        value={value}
+        onChange={(e) => handleChange(e)} // Керування зміною значення
+        className={cls.nativeSelect}
+      >
+        <option value="" disabled hidden>
+          {placeholder}
+        </option>
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
