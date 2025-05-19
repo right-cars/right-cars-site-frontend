0268a279ea3503db68b0301c4b665dceb480eb9e
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-19 11:20:49 +0200

    fix

--- src/views/HomeView/About/Team/FounderBlock/FounderBlock.tsx
+++ src/views/HomeView/About/Team/FounderBlock/FounderBlock.tsx
@@ -6,10 +6,7 @@ import cls from "./styles.module.scss";
 
 const data = {
   img: "/images/home/about/img1.png",
-      position: "hr Manager:",
-    name: "janene",
-    descr:
-  "empathetic leader fostering growth and a thriving workplace culture",
+      name: "Eugene Vulakh",
 };
 
 export default function FounderBlock() {
@@ -17,7 +14,7 @@ export default function FounderBlock() {
     <div className={cls.wrapper}>
       <div className={cls.infoBlock}>
         <div className={cls.imgBlock}>
-          <Image src={data.img} alt={data.position} width={150} height={160} className={cls.img} />
+          <Image src={data.img} alt={data.name} width={150} height={160} className={cls.img} />
           {/*<Image src="/images/home/banner/img1.webp" alt="some text" width="238" height="238" />*/}
           {/*<Image*/}
           {/*  src="/images/home/car-page/img1.png"*/}
