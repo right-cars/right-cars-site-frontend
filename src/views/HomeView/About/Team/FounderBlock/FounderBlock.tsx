d429a1e88467929984c5ab9feacf05d55f24bc9d
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-19 10:47:49 +0200

    fix

--- src/views/HomeView/About/Team/FounderBlock/FounderBlock.tsx
+++ src/views/HomeView/About/Team/FounderBlock/FounderBlock.tsx
@@ -9,13 +9,14 @@ export default function FounderBlock() {
     <div className={cls.wrapper}>
       <div className={cls.infoBlock}>
         <div className={cls.imgBlock}>
-          <Image
-            src="/images/home/car-page/img1.png"
-            alt="Eugene Vulakh"
-            width={238}
-            height={238}
-            className={cls.img}
-          />
+          <Image src="/images/home/banner/img1.webp" alt="some text" width="238" height="238" />
+          {/*<Image*/}
+          {/*  src="/images/home/car-page/img1.png"*/}
+          {/*  alt="Eugene Vulakh"*/}
+          {/*  width={238}*/}
+          {/*  height={238}*/}
+          {/*  className={cls.img}*/}
+          {/*/>*/}
         </div>
         <div className={cls.txtBlock}>
           <h3 className={`${"btnText"} ${cls.title}`}>
