bdfd071766314009eb855839ab7d6e197b439cb6
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-06-18 13:26:16 +0200

    fix

--- src/shared/components/FileUpload/tooltipList.ts
+++ src/shared/components/FileUpload/tooltipList.ts
@@ -2,9 +2,7 @@ export const tooltipList = [
   "Water, electricity, gas, telephone, or Internet bill",
   "Credit card bill or statement",
   "Bank statement",
-  "Bank reference letter",
   "Mortgage statement or contract",
   "Letter issued by a public authority",
-  "Company payslip",
   "Car or home insurance policy",
 ];
