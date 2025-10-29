8793c41b3e1684f2d6b2cea1cdf58a28276ca064
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-10-29 08:44:25 +0100

    fix

--- src/views/HomeView/About/Team/TeamList/data.ts
+++ src/views/HomeView/About/Team/TeamList/data.ts
@@ -55,11 +55,4 @@ export const data = [
     descr:
         "driven negotiator delivering tailored solutions with precision",
   },
-  // {
-  //   img: "/images/home/about/img10.jpg",
-  //   position: "Sales Manager:",
-  //   name: "JP",
-  //   descr:
-  //       "driven negotiator delivering tailored solutions with precision",
-  // },
 ];
